ccb729bac1f6ad124bad3e00365f7aab8a3dfcec
zNugget <81307155+zNugget@users.noreply.github.com>
2022-06-17 11:19:45 -0400

    Delete uv directory

--- uv/uv.config.js
+++
@@ -1,2 +0,0 @@
-const split=location.hostname.split('.');const domain=split.slice(0).slice(-(split.length===4?3:2)).join('.')
-self.__uv$config={prefix:'/service/',bare:'https://client.'+domain+'/',encodeUrl:Ultraviolet.codec.xor.encode,decodeUrl:Ultraviolet.codec.xor.decode,handler:'/uv/uv.handler.js',bundle:'/uv/uv.bundle.js',config:'/uv/uv.config.js',sw:'/uv/uv.sw.js',};
